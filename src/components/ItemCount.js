605538a3d27ef192f81f076413d236b6dce87442
ivinapoli218 <ivinapoli218@gmail.com>
2022-03-21 14:05:26 -0300

    Desafío Sincronizar Counter

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -1,36 +1,36 @@
 
-// import {Button} from "react-bootstrap"
-// import {useState} from "react"
+import {Button} from "react-bootstrap"
+import {useState} from "react"
 
-// const ItemCount = (props) => {
-//     const [contador, setContador] = useState(props.initial)
+const ItemCount = (props) => {
+    const [contador, setContador] = useState(props.initial)
 
-//     const sumar = () => {
-//         if (contador < props.stock) {
-//             setContador (contador + 1)
-//         }
-//     }
+    const sumar = () => {
+        if (contador < props.stock) {
+            setContador (contador + 1)
+        }
+    }
 
-//     const restar = () => {
-//         if (contador <= props.stock && contador > 0) {
-//             setContador (contador - 1)
-//         }
-//     }
+    const restar = () => {
+        if (contador <= props.stock && contador > 0) {
+            setContador (contador - 1)
+        }
+    }
 
-//     const confirmar = () => {
-//         setContador(0)
-//     }
+    const confirmar = () => {
+        props.onAdd(contador)
+    }
 
-//     return (
-//         <section>
-//             <h3>Producto x</h3>
-//             <h5>Stock: {props.stock}</h5>
-//             <h4>Cantidad: {contador}</h4>
-//             <Button onClick={sumar} variant="info" className="botonMas">+</Button>
-//             <Button onClick={restar} variant="info" className="botonMenos">-</Button>
-//             <Button onClick={confirmar} variant="info" className="botonConfirmar">confirmar</Button>
-//         </section>
-//     );
-// };
+    return (
+        <div>
+            <h5 className="countStock">Stock: {props.stock}</h5>
+            <h4 className="countCantidad">Cantidad: {contador}</h4>
+            <Button onClick={sumar} disabled={contador === props.stock} variant="info" className="botonMas">+</Button>
+            <Button onClick={restar} disabled={contador === 0} variant="info" className="botonMenos">-</Button>
+            <Button onClick={confirmar} disabled={contador === 0} variant="info" className="botonConfirmar">confirmar</Button>
+        </div>
+    );
+};
 
-// export default ItemCount;
+export default ItemCount;
+
--- src/components/ItemDetail.js
+++ src/components/ItemDetail.js
@@ -1,9 +1,21 @@
 
-import {Card} from "react-bootstrap"
+import ItemCount from "./ItemCount"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import {Button, Card} from "react-bootstrap";
 
 export const ItemDetail = (props) => {
     const producto = props.producto;
     const { id, nombre, precio, stock, categoria, descripcion, img } = producto;
+    const [contadorFuera, setContadorFuera] = useState(false);
+    const [unidadesCompradas, setUnidadesCompradas] = useState(0);
+
+    const onAdd = (contadorActivo) => {
+        if (contadorActivo != undefined) {
+            setContadorFuera(true);
+            setUnidadesCompradas(contadorActivo);
+        }
+    }
 
     return (
         <div className="flexDetailDiv">
@@ -15,8 +27,10 @@ export const ItemDetail = (props) => {
                     <Card.Body>
                         <Card.Title className="detailTitle">{nombre}</Card.Title>
                         <Card.Subtitle className="detailSubTitle">{descripcion}</Card.Subtitle>
-                        <Card.Text className="detailStock"> Stock disponible: {stock}</Card.Text>
                         <Card.Text className="detailPrice"> Precio: ${precio}</Card.Text>
+                        {contadorFuera ?
+                        <Link to="/carrito"><Button variant="info" className="botonIrAlCarrito">Ir al Carrito</Button></Link> :
+                        <ItemCount initial={1} stock={stock} onAdd={onAdd}/>}
                     </Card.Body>
                 </Card>
             </Card>
